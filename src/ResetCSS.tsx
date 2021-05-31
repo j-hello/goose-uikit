be09d1393fcb7ee2f166536054346b3c9bb127d9
j-hello <jessica@hellobear.co>
2021-05-31 18:12:05 +1000

    testing

--- src/ResetCSS.tsx
+++ src/ResetCSS.tsx
@@ -64,7 +64,7 @@ const ResetCSS = createGlobalStyle`
     box-sizing: border-box;
   }
   * {
-    font-family: 'Kanit', sans-serif;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
