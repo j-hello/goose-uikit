d174ecc597937fc77086ecfcaccd3ff46419f18b
j-hello <jessica@hellobear.co>
2021-06-08 00:35:33 +1000

    Egg houses

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -640,13 +640,13 @@ var ButtonMenu = function (_a) {
 };
 
 var InactiveButton = styled__default['default'](Button)(templateObject_1$8 || (templateObject_1$8 = __makeTemplateObject(["\n  color: ", ";\n"], ["\n  color: ", ";\n"])), function (_a) {
-    var theme = _a.theme, colorKey = _a.colorKey;
-    return theme.colors[colorKey];
+    var theme = _a.theme, colorkey = _a.colorkey;
+    return theme.colors[colorkey];
 });
 var ButtonMenuItem = function (_a) {
     var _b = _a.isActive, isActive = _b === void 0 ? false : _b, _c = _a.size, size = _c === void 0 ? sizes.MD : _c, _d = _a.variant, variant = _d === void 0 ? variants.PRIMARY : _d, as = _a.as, props = __rest(_a, ["isActive", "size", "variant", "as"]);
     if (!isActive) {
-        return (React__default['default'].createElement(InactiveButton, __assign({ forwardedAs: as, size: size, variant: "secondary", colorKey: variant === variants.PRIMARY ? "primary" : "textSubtle" }, props)));
+        return (React__default['default'].createElement(InactiveButton, __assign({ forwardedAs: as, size: size, variant: "secondary", colorkey: variant === variants.PRIMARY ? "primary" : "textSubtle" }, props)));
     }
     return React__default['default'].createElement(Button, __assign({ as: as, size: size, variant: variant }, props));
 };
--- src/components/ButtonMenu/ButtonMenuItem.tsx
+++ src/components/ButtonMenu/ButtonMenuItem.tsx
@@ -5,11 +5,11 @@ import { sizes, variants } from "../Button/types";
 import { ButtonMenuItemProps } from "./types";
 
 type InactiveButtonProps = {
-  colorKey: "primary" | "textSubtle";
+  colorkey: "primary" | "textSubtle";
 } & ButtonMenuItemProps;
 
 const InactiveButton = styled(Button)<InactiveButtonProps>`
-  color: ${({ theme, colorKey }) => theme.colors[colorKey]};
+  color: ${({ theme, colorkey }) => theme.colors[colorkey]};
 `;
 
 const ButtonMenuItem: React.FC<ButtonMenuItemProps> = ({
@@ -25,7 +25,7 @@ const ButtonMenuItem: React.FC<ButtonMenuItemProps> = ({
         forwardedAs={as}
         size={size}
         variant="secondary"
-        colorKey={variant === variants.PRIMARY ? "primary" : "textSubtle"}
+        colorkey={variant === variants.PRIMARY ? "primary" : "textSubtle"}
         {...props}
       />
     );
